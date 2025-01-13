3c81589399816d4ee1a3753ae23f3d6db9944a05
석영민 <ymseok@marblex.net>
2025-01-13 19:15:28 +0900

    서비스 도메인 정보 설정 구간 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
         publishableKey: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY as string,
       },
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID as string,
-      redirectUri: "http://localhost:3000/redirect",
-      logoutRedirectUri: "http://localhost:3000/",
+      redirectUri: `${process.env.SERVICE_DOMAIN}/redirect`,
+      logoutRedirectUri: `${process.env.SERVICE_DOMAIN}/`,
       audience: "platform_api",
       scope: "openid offline_access email transact",
     });
